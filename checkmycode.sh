c9092e4ad37e3975bdc16c9d89d5c62a79c6f5f2
Sudeep Prasad <sudeep.rp@numocity.com>
2022-01-12 09:46:37 +0530

    fix coverage exceptions

--- checkmycode.sh
+++ checkmycode.sh
@@ -6,23 +6,19 @@ NC='\033[0m'
 jscpd --min-lines 3 --min-tokens 50 --threshold 0 --gitignore --ignore "**/*.xml,**/*.json"
 flutter analyze
 flutter test --coverage
-flutter pub run test_cov_console
 
-if (( $(echo "$(wc -l < lib/main.dart) > 7" | bc -l) )); then
-  echo -e "${RED}main.dart has more than 7 lines${NC}. It is not covered so keep it small."
+echo
+filesWithoutFuncs=$(grep -rLE "\)\s*{" ./lib | sed 's/^.\///g' | tr '\n' , | sed 's/,$//g')
+echo "Files without functions, coverage ignored:"
+echo $filesWithoutFuncs
+
+flutter pub run test_cov_console --exclude=$filesWithoutFuncs
+flutter pub run test_cov_console --pass=100 | grep -q PASS
+
+flutter pub run test_cov_console --csv --exclude=$filesWithoutFuncs
+if grep -q "no unit test" coverage/test_cov_console.csv; then
+  echo -e "${RED}Some files aren't covered${NC}"
   exit 1
 fi
 
-flutter pub run test_cov_console -c
-if grep -v main.dart coverage/test_cov_console.csv | grep -q "no unit test"; then
-  echo -e "${RED}Some files aren't covered${NC}: Cover everything except main.dart"
-  exit 1
-fi
-
-export "IFS=,"
-read label br fn li un <<< $(grep "All files" coverage/test_cov_console.csv)
-if (( $(echo "$br < 100" | bc -l) )) || (( $(echo "$fn < 100" | bc -l) )) || (( $(echo "$li < 100" | bc -l) )); then
-  echo -e "${RED}Go for full coverage${NC} - you can do it (all except main.dart)"
-  exit 1
-fi
 echo Check Complete :\)
